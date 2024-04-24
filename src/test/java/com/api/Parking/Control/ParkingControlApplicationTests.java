484fd038b68ea9e0980724d72ed6124c26c5db80
Pedro Henrique <ph8584774@gmail.com>
2024-04-24 20:19:49 -0300

    Project Created

---
+++ src/test/java/com/api/Parking/Control/ParkingControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.Parking.Control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
