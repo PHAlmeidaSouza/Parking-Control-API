8913dc5d6deb7160181a95f14bfe3c7865a38559
Pedro Henrique <ph8584774@gmail.com>
2024-04-25 23:19:29 -0300

    Pagination

--- src/main/java/com/api/Parking/Control/controllers/ParkingSpotController.java
+++ src/main/java/com/api/Parking/Control/controllers/ParkingSpotController.java
@@ -5,6 +5,10 @@ import com.api.Parking.Control.models.ParkingSpotModel;
 import com.api.Parking.Control.services.ParkingSpotService;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -47,10 +51,8 @@ public class ParkingSpotController {
     }
 
     @GetMapping
-    public ResponseEntity<List<ParkingSpotModel>> getAllParkingSports() {
-
-        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll());
-
+    public ResponseEntity<Page<ParkingSpotModel>> getAllParkingSpots(@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll(pageable));
     }
 
     @GetMapping("/{id}")
