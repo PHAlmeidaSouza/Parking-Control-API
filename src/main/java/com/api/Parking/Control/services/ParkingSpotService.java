0701d290b0ca0b7b845fe703be3e0921b18cd04e
Pedro Henrique <ph8584774@gmail.com>
2024-04-25 22:20:30 -0300

    POST Parking Spot created

--- src/main/java/com/api/Parking/Control/services/ParkingSpotService.java
+++ src/main/java/com/api/Parking/Control/services/ParkingSpotService.java
@@ -1,13 +1,21 @@
 package com.api.Parking.Control.services;
 
+import com.api.Parking.Control.models.ParkingSpotModel;
 import com.api.Parking.Control.repositories.ParkingSpotRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ParkingSpotService {
-    @Autowired
-    private ParkingSpotRepository parkingSpotRepository;
 
-    
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
+    }
 }
